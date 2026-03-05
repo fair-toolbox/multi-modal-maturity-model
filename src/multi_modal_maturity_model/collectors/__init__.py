483200d4bbd1e8fd9a218a773a55f82842b52041
anitamnd <anamendesml@outlook.com>
2026-03-05 15:58:13 +0100

    add GitlabCollector to the module exports

--- src/multi_modal_maturity_model/collectors/__init__.py
+++ src/multi_modal_maturity_model/collectors/__init__.py
@@ -1,7 +1,8 @@
 from .biotools import BioToolsCollector
 from .lizard import CodeQualityCollector
 from .github import GitHubCollector
+from .gitlab import GitlabCollector
 from .europe_pmc import EuropePMCCollector
 from .howfairis import FairnessCollector
 
-__all__ = ["BioToolsCollector", "CodeQualityCollector", "GitHubCollector", "EuropePMCCollector", "FairnessCollector"]
+__all__ = ["BioToolsCollector", "CodeQualityCollector", "GitHubCollector", "GitlabCollector", "EuropePMCCollector", "FairnessCollector"]
