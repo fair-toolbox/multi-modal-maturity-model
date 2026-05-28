4f74e47011f07efd76f1721fe8554c6713673465
anitamnd <anamendesml@outlook.com>
2026-05-28 19:00:53 +0200

    add client imports

---
+++ src/multi_modal_maturity_model/clients/__init__.py
@@ -0,0 +1,6 @@
+from .biotools import BioToolsClient
+from .github import GitHubClient
+from .gitlab import GitLabClient
+from .openalex import OpenAlexClient
+
+__all__ = ["GitHubClient", "GitLabClient", "OpenAlexClient", "BioToolsClient"]
